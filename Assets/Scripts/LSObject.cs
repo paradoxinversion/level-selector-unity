f4ce8c4fb8bcc56ea684664e46d41b2ed569b6f0
Jedai Saboteur <dev@paradoxinversion.com>
2015-10-08 10:44:18 -0700

    Initial Commit

---
+++ Assets/Scripts/LSObject.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class LSObject : MonoBehaviour{
+    public int unitySceneIndex;
+    public int levelDisplayIndex;
+    public string levelName;
+    public string levelDescription;
+
+    public Image mainImage;
+
+    public Text levelNameText;
+    public Text levelIndexText;
+    public Text levelDescriptionText;
+
+    public void SetText()
+    {
+        if (!string.IsNullOrEmpty(levelName))
+        {
+            if (levelName != null)
+            { levelNameText.text = levelName; }
+            
+        }
+        if (!string.IsNullOrEmpty(levelDescription))
+        {
+            if (levelDescriptionText != null)
+            {
+                levelDescriptionText.text = levelDescription;
+            }
+            
+        }
+        if (levelDisplayIndex > -1)
+        {
+            if (levelIndexText != null)
+            {
+                levelIndexText.text = levelDisplayIndex.ToString();
+            }
+            
+        }
+    }
+
+    public void LoadLevel()
+    {
+        Application.LoadLevel(unitySceneIndex);
+    }
+    void Start()
+    {
+        SetText();
+    }
+}
